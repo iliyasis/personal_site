a10d0d7ade922ddca9eab20eb28a6a260f70ff27
MirzaSis <webiliya1504@gmail.com>
2026-07-04 16:00:31 +0330

    back

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,15 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = "django-insecure-@09=pstd)4xwhr)v7_pq5p^ycx82!ktf3h$vbwx0r0mz2%5nml"
-SECRET_KEY = os.environ.get('SECRET_KEY', 'یه-مقدار-پیش‌فرض-فقط-برای-لوکال')
+SECRET_KEY = "django-insecure-@09=pstd)4xwhr)v7_pq5p^ycx82!ktf3h$vbwx0r0mz2%5nml"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
-#ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
+ALLOWED_HOSTS = []
 
 
 
@@ -49,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -124,19 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = "static/"
-# STATICFILES_DIRS = [
-#     BASE_DIR / "assets",
-# ]
-# STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "assets",
+]
+STATIC_ROOT = BASE_DIR / "static"
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
